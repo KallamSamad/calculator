6438ed4e26739c31d2674abdc216331d33dccedb
KallamSamad <kallamsamad@gmail.com>
2025-08-13 12:38:45 +0100

    added the correct divs for each button so i can style with css

--- calculator.js
+++ calculator.js
@@ -1,12 +1,62 @@
+let operators = ["x","+","-","÷"]
+
+const outer = document.createElement("div")
+outer.classList.add("outer")
+document.body.appendChild(outer)
+
 const calc = document.createElement("div")
-document.body.appendChild(calc)
+calc.classList.add("calc")
+outer.appendChild(calc)
+
 
 const buttonCont = document.createElement("div")
+buttonCont.classList.add("buttonCont")
 calc.appendChild(buttonCont)
 
-for (let x=0; x<10; x++){
+
+const numberWrapper = document.createElement("div");
+numberWrapper.classList.add("numberWrapper");
+buttonCont.appendChild(numberWrapper);
+
+
+const numberCont = document.createElement("div")
+numberCont.classList.add("numberCont")
+buttonCont.appendChild(numberCont)
+
+
+const bottomCont = document.createElement("div")
+bottomCont.classList.add("bottomCont")
+buttonCont.appendChild(bottomCont)
+
+const zeroBtn = document.createElement("button")
+zeroBtn.textContent = "0"
+zeroBtn.classList.add("numberBtns")
+bottomCont.appendChild(zeroBtn)
+
+const equalBtn = document.createElement("button")
+equalBtn.textContent = "="
+equalBtn.classList.add("operator")
+bottomCont.appendChild(equalBtn)
+
+numberWrapper.appendChild(numberCont);
+numberWrapper.appendChild(bottomCont);
+
+
+
+for (let x=1; x<10; x++){
     const numbers = document.createElement("button")
     numbers.textContent= `${x}`
     numbers.classList.add("numberBtns")
-    buttonCont.appendChild(numbers)
+    numberCont.appendChild(numbers)
+}
+
+const operatorCont =document.createElement("div")
+operatorCont.classList.add("operatorCont")
+buttonCont.appendChild(operatorCont)
+
+for (let x=0; x<4;x++){
+    const operator=document.createElement("button")
+    operator.classList.add("operator")
+    operator.textContent = `${operators[x]}`
+    operatorCont.appendChild(operator)
 }
